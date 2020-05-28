6ff8fe82de76cc7517fb782396823e81685744c3
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-28 18:36:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,9 +51,9 @@ def turn(board)
   index = input_to_index(input)
     
     if valid_move?(board,index) 
-      move(board, index, "X")
+      move(board, index, current_player(board))
       display_board(board)
-      puts "Please enter 1-9:"
+      
     else 
       turn(board)
   end
