fd894458395018b0b13b7018ebce59091b4783ad
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-28 18:45:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,11 +28,7 @@ def move(board,index,char)
 end
 
 def valid_move?(board,index)
-  if index.between?(0,8) && !position_taken?(board,index)
-    return true
-  else
-    return false
-  end
+  index.between?(0,8) && !position_taken?(board,index)
 end  
 
 def position_taken?(board,index)
